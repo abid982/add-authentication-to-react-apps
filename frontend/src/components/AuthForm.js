52da43bf83134d6c6df78c41cef4cca43b7b1be1
abid982 <abidarif982@gmail.com>
2023-03-03 16:57:12 +0500

    implemented auth action, validating user input and outputting validation errors and adding user login

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,9 +1,19 @@
 // import { useState } from 'react';
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
+  const data = useActionData();
+  console.log('Data:');
+  console.log(data);
+
   // Two elements in an array
   // First is an object that gives us access to the currently set query parameters.
   // Second is a function that allows us to update the currently set query parameters.
@@ -15,10 +25,15 @@ function AuthForm() {
   // Retrieve value of mode parameter
   // Returns a Boolean Value
 
-  console.log()
+  console.log();
   const isLogin = searchParams.get('mode') === 'login';
   console.log('Is login:');
   console.log(isLogin);
+
+  // The useNavigation() hook
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
 
   // const [isLogin, setIsLogin] = useState(true);
 
@@ -48,32 +63,49 @@ function AuthForm() {
     // </>
 
     <>
-    <Form method="post" className={classes.form}>
-      <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
-      <p>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" required />
-      </p>
-      <p>
-        <label htmlFor="image">Password</label>
-        <input id="password" type="password" name="password" required />
-      </p>
-      <div className={classes.actions}>
+      <Form method="post" className={classes.form}>
+        {/* If we have data and errors */}
+        {/* Loop through all errors */}
+        {/* Loop through error object with the built in JavaScript Object */}
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map(error => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+
+        {/* If data has message property on this data object */}
+
+        {data && data.message && <p>{data.message}</p>}
+
+        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <p>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" required />
+        </p>
+        <p>
+          <label htmlFor="image">Password</label>
+          <input id="password" type="password" name="password" required />
+        </p>
+        <div className={classes.actions}>
           {/* <Link to="?mode=signup"> */}
-        {/* <Link to="?mode=login"> */}
-            {/* {isLogin ? 'Create new user' : 'Login'} */}
-            {/* Opposite mode */}
+          {/* <Link to="?mode=login"> */}
+          {/* {isLogin ? 'Create new user' : 'Login'} */}
+          {/* Opposite mode */}
           {/* If we are on login then setup to signup and vice-versa */}
           {/* Setup dynamically */}
           {/* Manage mode with query parameter */}
           {/* If isLogin is true i.e mode===login then the new mode should be set to signup other set to login */}
           <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-        </Link>
-        <button>Save</button>
-      </div>
-    </Form>
-  </>
+          </Link>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Save'}
+          </button>
+        </div>
+      </Form>
+    </>
   );
 }
 
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { json, redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,133 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+// Create an action
+// The action function will be triggered whenever this AuthForm is submitted because it is on the same route as this AuthForm is on
+// We must get the form data that was submitted
+// The data package is given by react-router when the form is submitted
+export async function action({ request, params }) {
+  console.log('Request:');
+  console.log(request);
+
+  console.log('Params:');
+  console.log(params);
+
+  // Hold data with formData() method
+  const data = await request.formData();
+
+  // Now of course, we wanna send different requests based on the mode this form is in if we're in login or signup mode. So therefore we also must take a look at this query parameter in our action to find our whether a signup or login request should be sent. But the useSearch() hook is not working here because we're not in the Component Function
+
+  // But we can use the built-in URL Constructor which is provided by the browser and pass our request.url to it
+
+  // The URL() constructor returns a newly created URL object representing the URL defined by the parameters.
+
+  // If the given base URL or the resulting URL are not valid URLs, the JavaScript TypeError exception is thrown.
+
+  // Default browser feature
+  const searchParams = new URL(request.url).searchParams;
+
+  console.log('URL:');
+  console.log(new URL(request.url));
+
+  console.log('Search params:');
+  console.log(searchParams);
+  console.log(new URL('http://localhost:3001/auth?mode=login').searchParams);
+
+  const mode = searchParams.get('mode') || 'login';
+  console.log(mode);
+
+  const email = data.get('email');
+  console.log(email);
+
+  console.log('Form data:');
+  console.log(data);
+
+  const password = data.get('password');
+
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  console.log(authData);
+
+  // Check mode if incorrect return
+  if (mode !== 'login' && mode !== 'signup') {
+    // Throw a new error response and import the json funciton from react-router-dom
+    // For invalid input status 422
+    console.log('TEST:');
+    throw json({ message: 'Unsupported mode.' }, { status: 422 });
+  }
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(authData),
+  });
+
+  console.log('Response:');
+  console.log(response);
+
+  // Code to handle response
+  // Validation errors
+  // 401 Invalid error from backend for example if we try to login with invalid credentials.
+  if (response.status === 422 || response.status === 401) {
+    // Return data to route component so that we can show the message there and show the validation errors next to the form for example
+    // React router will automatically extract the data for you
+    console.log('Testing...');
+    return response;
+  }
+  // If we have any other error
+  //   The 500 Internal Server Error is a very general HTTP status code
+  // that means something has gone wrong on the web site's server
+  // but the server could not be more specific on what the exact problem is.
+
+  // Simple: I don't know what the **** just went wrong
+  if (!response.ok) {
+    // Throw error response so that our closest error element is rendered on the screen
+    throw json({ message: 'Could not authenticate user.' }, { status: 500 });
+  }
+
+  // Now if we make it past all these steps here the user creation or signup did succeed.
+  // Soon: Manage that token
+
+  // Just redirect the user
+  // So once the user logged in redirect the user to homepage
+  return redirect('/');
+
+  //   And if we now try to create a user,
+
+  // with the same email as before, the password does not matter.
+
+  // But if it's the same email as before,
+
+  // you will see if I click save, nothing happens.
+
+  // The reason for that is that, behind the scenes,
+
+  // we're actually getting authentication errors,
+
+  // because the backend checks
+
+  // if a user with that email exists already,
+
+  // and if that's the case,
+
+  // it sends back an authentication error,
+
+  // with status code 422.
+
+  // And therefore,
+
+  // now we just want to handle this on the frontend,
+
+  // to show an appropriate error message,
+
+  // because it's all working as it should,
+
+  // but we are not giving the user the feedback
+  // he or she should be getting.
+}
